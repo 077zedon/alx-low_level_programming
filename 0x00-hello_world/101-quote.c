4d96de43b88c0265548756f010462b7f623cd9c7
077zedon <kjohnmark98@gmail.com>
2023-09-08 00:21:51 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+101-quote.c
+#include <s tdio.h>
+#include <unistd.h>
+/**
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
+int main(void)
+{
+        write(2,
+        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
+}
